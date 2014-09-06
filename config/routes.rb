65005896b8b5421904ff9c9250b7dab67c0d989a
Eric Stensland <estensland88@gmail.com>
2014-09-06 08:16:09 -0500

    reworking towards new end

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   root "site#index"
   get "posts/" => "posts#index"
   get "posts/:id" => "posts#show"
+  get "posts/:post_id/comments/:id" => "comments#show"
+  
+  get "comments/" => "comments#index"
+
 
   get "tags/" => "tags#index"
   get "tags/:id" => "tags#show"
 
-  get "comments/" => "comments#index"
-  get "comments/:id" => "comments#show"
 
   post "fs/" => 'flying_switch#async'
 
